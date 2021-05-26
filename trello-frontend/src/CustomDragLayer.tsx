e3f2f4fc07c2dc02d10a24ab292dd7e5dd8ad584
smarttin <smarttin77@gmail.com>
2021-05-26 20:43:56 +0100

    basic card drag n drop

---
+++ trello-frontend/src/CustomDragLayer.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { XYCoord, useDragLayer } from 'react-dnd';
+import { Column } from './Column';
+import { CustomDragLayerContainer } from './styles';
+import { DragItem } from './DragItem';
+import { Card } from './Card';
+
+export const CustomDragLayer = () => {
+  const { isDragging, currentOffset, item } = useDragLayer((monitor) => ({
+    isDragging: monitor.isDragging(),
+    currentOffset: monitor.getSourceClientOffset(),
+    item: monitor.getItem() as DragItem,
+  }));
+
+  const getItemStyles = (currentOffset: XYCoord | null): React.CSSProperties => {
+    if (!currentOffset) {
+      return {
+        display: 'none',
+      };
+    }
+    const { x, y } = currentOffset;
+    const transform = `translate(${x}px, ${y}px)`;
+    return {
+      transform,
+      WebkitTransform: transform,
+    };
+  };
+
+  return isDragging ? (
+    <CustomDragLayerContainer>
+      <div style={getItemStyles(currentOffset)}>
+        {item.type === 'COLUMN' ? (
+          <Column id={item.id} text={item.text} index={item.index} isPreview={true} />
+        ) : (
+          <Card columnId={item.columnId} isPreview={true} index={0} id={item.id} text={item.text} />
+        )}
+      </div>
+    </CustomDragLayerContainer>
+  ) : null;
+};
